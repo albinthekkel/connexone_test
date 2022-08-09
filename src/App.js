b175a8a8509abf281610dd5e413fa22c1ac67c56
albinbaby <albinbaby@192.168.8.105>
2022-08-09 12:53:34 +0100

    first commit front end

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Timer / Metrics</h2>
+      <div className="timer-container">
+        <div className="col-lgl12 row mb-5">
+          <div className="col-lg-6 rel-left">hi</div>
+
+          <div className="col-lg-6">hi</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
